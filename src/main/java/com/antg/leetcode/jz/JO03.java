3ef6594bbcc2753c048cd7919f5219a5ea5d68c9
antg <798940311@qq.com>
2023-05-24 18:06:03 +0800

    [jz] jz 03题 数组中重复的数字

--- src/main/java/com/antg/leetcode/jz/JO03.java
+++ src/main/java/com/antg/leetcode/jz/JO03.java
@@ -1,4 +1,7 @@
-package com.antg.leetcode.jz_offer;
+package com.antg.leetcode.jz;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * <p>
@@ -10,13 +13,38 @@ package com.antg.leetcode.jz_offer;
  **/
 public class JO03 {
     public static void main(String[] args) {
-        int[] nums = {2, 3, 1, 0, 2, 5, 3};
-        int repeatNumber = findRepeatNumber(nums);
-        System.out.println(repeatNumber);
+        int[] nums = {3, 4, 2, 1, 1, 0};
+        int res1 = m1(nums);
+        System.out.println(res1);
+        int res2 = m2(nums);
+        System.out.println(res2);
     }
 
 
-    public static int findRepeatNumber(int[] nums) {
-        return 0;
+    /**
+     * @author Antg
+     * @description 双层for循环
+     * @time 2023/5/24 17:53
+     **/
+    public static int m1(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return nums[i];
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static int m2(int[] nums) {
+        Set<Integer> dic = new HashSet<>();
+        for (int num : nums) {
+            if (dic.contains(num)) {
+                return num;
+            }
+            dic.add(num);
+        }
+        return -1;
     }
 }
