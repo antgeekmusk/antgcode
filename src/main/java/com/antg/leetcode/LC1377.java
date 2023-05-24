3113ecd2116485f89dabb69365c196e404e4e1b2
antg <798940311@qq.com>
2023-05-24 18:05:13 +0800

    [jz] jz 03题 数组中重复的数字

--- src/main/java/com/antg/leetcode/LC1377.java
+++
@@ -1,7 +0,0 @@
-package com.antg.leetcode;
-
-public class LC1377 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/com/antg/leetcode/jz/JO03.java
@@ -0,0 +1,22 @@
+package com.antg.leetcode.jz_offer;
+
+/**
+ * <p>
+ * 剑指 Offer 03. 数组中重复的数字
+ * </p>
+ *
+ * @author Antg
+ * @since 2023/5/24 17:44
+ **/
+public class JO03 {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 0, 2, 5, 3};
+        int repeatNumber = findRepeatNumber(nums);
+        System.out.println(repeatNumber);
+    }
+
+
+    public static int findRepeatNumber(int[] nums) {
+        return 0;
+    }
+}
