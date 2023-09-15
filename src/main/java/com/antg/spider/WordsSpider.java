411a5f3c904210dd0709ef93c73d35a86666de5a
Antg <798940311@qq.com>
2023-09-15 14:17:17 +0800

    [spider] words spider

--- src/main/java/com/antg/spider/WordsSpider.java
+++ src/main/java/com/antg/spider/WordsSpider.java
@@ -11,6 +11,8 @@ import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
 * @author:Antg
@@ -20,26 +22,38 @@ import java.util.List;
 public class WordsSpider {
     public static void main(String[] args) throws Exception{
         List<Word> res = new ArrayList<>();
-        String filePath="";
-        String url="";
+        String filePath="~/cet6_words.txt";
+        String url="http://danci.gjcha.com/cet6_list/";
         Document doc = Jsoup.connect(url).get();
         Elements words = doc.select("#main_container > div > div.col-md-9 > div > div > div > div.word_list > a");
+
+        // 爬取100个就暂停1-10秒
+        int index=0;
         for (Element e : words) {
-            String word = e.text();
-
-
-
-
+            Word word = Word.builder().word(e.text()).build();
+            getWordDetail(word);
+            appendToFile(word.toString(),filePath);
+            index++;
+            if(index%100==0){
+                int stopRandom=new Random().nextInt(10)+1;
+                System.out.println("stop : "+stopRandom+" second ");
+                Thread.sleep(stopRandom*1000);
+                System.out.println("get data size : "+index);
+            }
         }
     }
 
-    public static void getWordDetail(Word word) throws Exception{
-        String url=""+word.getWord()+".html";
-        Document document = Jsoup.connect(url).get();
-        String explain = document.select("#main_container > div > div.col-md-9 > div > div > div.danci_info > p:nth-child(4)").text();
-        String pronounce = document.select("#main_container > div > div.col-md-9 > div > div > div.danci_info > div.yinbiao > p:nth-child(3) > span:nth-child(3)").text();
-        word.setExplain(explain);
-        word.setPronounce(pronounce);
+    public static void getWordDetail(Word word){
+        try {
+            String url = "http://danci.gjcha.com/" + word.getWord().replace("-", "____").replace(".","____") + ".html";
+            Document document = Jsoup.connect(url).get();
+            String explain = document.select("#main_container > div > div.col-md-9 > div > div > div.danci_info > p:nth-child(4)").text();
+            String pronounce = document.select("#main_container > div > div.col-md-9 > div > div > div.danci_info > div.yinbiao > p:nth-child(3) > span:nth-child(3)").text();
+            word.setExplain(explain);
+            word.setPronounce(pronounce);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void appendToFile(String word,String filePath){
@@ -60,7 +74,7 @@ public class WordsSpider {
         public String toString() {
             return word + " | "
                     +pronounce + " | "
-                    +explain
+                    +explain + "\n"
                     ;
         }
     }
